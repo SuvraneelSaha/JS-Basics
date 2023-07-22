bab2d7aa0b9e99ce4cc63baa327446109240e548
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-07-22 21:02:07 +0530

    completed before variables

---
+++ index.js
@@ -0,0 +1,42 @@
+// first js code 
+console.log("Hello World Im in Browser");
+
+
+// for running the js code in node , we use the terminal 
+// ex - node (filename).js -->it will run 
+// by default the js code can run in a browser 
+// example - in case of google - gooogl's V8 Javascript engine is used to execute / run the the js code 
+// in case of firefox spider monkey javascript engine is used to execute / run the code 
+
+// we can use node - node is an runtime enviroment for running or executing a js code 
+// node is actually a c++ program in which the google's V8 javascript engine has been embedded by which we can run the js code 
+//  outside of a browser , thus making backend , desktop and cross platform applications a new way of development by using javascript and 
+//  some forms of a javascript 
+
+
+let a ;
+console.log(a);
+
+// the default value of any undefined variable is undefined 
+// more exactyle un initialized variable's value = undefined 
+
+let name="Mosh";
+console.log(name);
+
+// VARIABLE NAMING RULES:
+// there are some rules regarding the variable naming - that is 
+// we cannot use reserved keywords of the js language 
+// cannot start with a number 
+// cannot contain a space in between the words 
+// cannot contain hyphen (-)
+// Follow camel case example 
+
+let camelCase; 
+// camelCase notation covention of writting variable name 
+
+// Rules 
+// variable names are case sensitive 
+
+// there is a difference in between decleration and initialization 
+
+// Multiple variable decleration 
